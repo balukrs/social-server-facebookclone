aac19a734cc043f085bd20e9789f5cb2eadd2437
BALU KRISHNA R <balukrs@outlook.com>
2021-05-19 20:15:28 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -87,7 +87,11 @@ router.post("/login", async (req, res) => {
     process.env.SECRET_KEY
   );
 
-  res.cookie("token", token, { httpOnly: true, secure: true });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    domain: ".herokuapp.com",
+  });
   res.status(200).send("loginsuccess");
 });
 
