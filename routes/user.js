7efc88ccae9a0b6214cb872ac14e7beeabc7b300
BALU KRISHNA R <balukrs@outlook.com>
2021-05-19 20:10:52 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -87,7 +87,7 @@ router.post("/login", async (req, res) => {
     process.env.SECRET_KEY
   );
 
-  res.cookie("token", token, { httpOnly: true });
+  res.cookie("token", token, { httpOnly: true, secure: true });
   res.status(200).send("loginsuccess");
 });
 
