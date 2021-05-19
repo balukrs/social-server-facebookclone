3bef0b15700d7632135e102acdfe22049fd84904
BALU KRISHNA R <balukrs@outlook.com>
2021-05-19 19:22:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "https://master.drdmxw5oqy6h3.amplifyapp.com/",
+    origin: "https://master.drdmxw5oqy6h3.amplifyapp.com",
     credentials: true,
   })
 );
